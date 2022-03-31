d0b6a7a49d57428df5d9f79438f66a1736c6871e
Natanael Caetano <natacaet@gmail.com>
2022-03-31 17:53:20 -0300

    update

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,7 +3,6 @@ import { createContext, useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
 import firebase from '../firebase/config'
 import Usuario from '../model/Usuario'
-import Firebase from './Firebase';
 
 interface AuthContextProps {
     usuario?: Usuario
