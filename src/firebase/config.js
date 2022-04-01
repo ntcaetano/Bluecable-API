43d3f8a43ef455fb344c16d99bb20c30eb78d25b
Natanael Caetano <natacaet@gmail.com>
2022-04-01 09:59:01 -0300

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,7 +4,11 @@ import 'Firebase/auth';
 const FirebaseCredentials = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 if (!Firebase.apps.length) {
   Firebase.initializeApp(FirebaseCredentials)
