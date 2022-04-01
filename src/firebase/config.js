a5491f53fc2a736484ede62ac6b7ecae2e0486df
Natanael Caetano <natacaet@gmail.com>
2022-04-01 10:20:47 -0300

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,13 @@
-import Firebase from 'Firebase/app';
-import 'Firebase/auth';
+import firebase from "firebase/app";
+import 'firebase/auth'
 
-const FirebaseCredentials = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-}
-if (!Firebase.apps.length) {
-  Firebase.initializeApp(FirebaseCredentials)
+if(!firebase.apps.lenght){
+    firebase.initializeApp({
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    })
 }
 
-export default Firebase;
+export default firebase
